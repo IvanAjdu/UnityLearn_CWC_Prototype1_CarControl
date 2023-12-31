668968734644b87ad523328c3e15bc2d59b84c88
IvanAjdu <ivan.ajdu@gmail.com>
2024-01-01 00:12:12 +0100

    Input player script; Change in move values

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,10 +1,11 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] float speed = 15.0f;
+    [SerializeField] float turnSpeed = 30f;
+    [SerializeField] float horizontalInput;
+    [SerializeField] float verticalInput;
 
     // Start is called before the first frame update
     void Start()
@@ -15,7 +16,12 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        //get Input for movements
+        horizontalInput = Input.GetAxis("Horizontal");
+        verticalInput = Input.GetAxis("Vertical");
         //we'll move the vehicle forward
-        transform.Translate(Vector3.forward*Time.deltaTime*speed);
+        transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
+        //rotate the vehicle
+        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
     }
 }
