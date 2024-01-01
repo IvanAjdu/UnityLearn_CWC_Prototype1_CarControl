fb77f6d50070c64321857366a1c74c1b420743b6
IvanAjdu <ivan.ajdu@gmail.com>
2024-01-01 15:01:47 +0100

    Challenge 1 finished; debug ok; propeller with gameObject attribution in PlayerController script.

--- Assets/Challenge 1/Scripts/PlayerControllerX.cs
+++ Assets/Challenge 1/Scripts/PlayerControllerX.cs
@@ -4,9 +4,10 @@ using UnityEngine;
 
 public class PlayerControllerX : MonoBehaviour
 {
-    public float speed;
-    public float rotationSpeed;
+    public float speed = 0.5f;
+    public float rotationSpeed = 100;
     public float verticalInput;
+    public GameObject propeller;
 
     // Start is called before the first frame update
     void Start()
@@ -21,9 +22,12 @@ public class PlayerControllerX : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
 
         // move the plane forward at a constant rate
-        transform.Translate(Vector3.back * speed);
+        transform.Translate(Vector3.forward * speed);
+
+        // rotate the propeller
+        propeller.transform.Rotate(Vector3.forward*50);
 
         // tilt the plane up/down based on up/down arrow keys
-        transform.Rotate(Vector3.right * rotationSpeed * Time.deltaTime);
+        transform.Rotate(Vector3.right, rotationSpeed * Time.deltaTime * verticalInput);
     }
 }
