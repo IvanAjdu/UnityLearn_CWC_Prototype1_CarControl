a781d5ba5614af870a538607225d7b5cd6f41a58
IvanAjdu <ivan.ajdu@gmail.com>
2024-07-12 12:52:46 +0200

    speedometer : workinprogress (check playercontroller script)

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,11 +1,20 @@
+using TMPro;
+using Unity.VisualScripting;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField] float speed = 15.0f;
+    [SerializeField] float speed;
     [SerializeField] float turnSpeed = 30f;
     [SerializeField] float horizontalInput;
     [SerializeField] float verticalInput;
+    private Rigidbody playerRb;
+    [SerializeField] TextMeshProUGUI speedometer;
+
+    private void Start()
+    {
+        playerRb = GetComponent<Rigidbody>();
+    }
 
     // Update is called once per frame
     void FixedUpdate()
@@ -17,5 +26,11 @@ public class PlayerController : MonoBehaviour
         transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
         //rotate the vehicle
         transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+
+        speed = playerRb.velocity.magnitude * 3.6f;
+        speedometer.SetText("Speed : " + speed + " km/h");
+
+        //ADD FORCE 
+        // ROUND KM/H
     }
 }
