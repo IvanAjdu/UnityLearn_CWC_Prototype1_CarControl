ae8df089b4984a4e967c6393c443e9f87957560c
IvanAjdu <ivan.ajdu@gmail.com>
2024-07-16 15:39:13 +0200

    Road detection with wheel colliders.

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,3 +1,4 @@
+using System.Collections.Generic;
 using TMPro;
 using Unity.VisualScripting;
 using UnityEngine;
@@ -5,12 +6,16 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     private float speed;
+    [SerializeField] float rpm;
     [SerializeField] float turnSpeed = 30f;
     [SerializeField] float horizontalInput;
     [SerializeField] float verticalInput;
     private Rigidbody playerRb;
     [SerializeField] float horsePower;
     [SerializeField] TextMeshProUGUI speedometer;
+    [SerializeField] TextMeshProUGUI rpmMeter;
+    [SerializeField] List<WheelCollider> wheels;
+    private int wheelsOnRoad;
 
     private void Start()
     {
@@ -25,11 +30,40 @@ public class PlayerController : MonoBehaviour
         verticalInput = Input.GetAxis("Vertical");
         //we'll move the vehicle forward
         //transform.Translate(Vector3.forward*Time.deltaTime*speed*verticalInput);
-        playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
-        //rotate the vehicle
-        transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
+        if (IsOnRoad())
+        {
+            playerRb.AddRelativeForce(Vector3.forward * horsePower * verticalInput);
+            //rotate the vehicle
+            transform.Rotate(Vector3.up, Time.deltaTime * turnSpeed * horizontalInput);
 
         speed = Mathf.RoundToInt(playerRb.velocity.magnitude * 3.6f);
         speedometer.SetText("Speed : " + speed + " km/h");
+
+        rpm = (speed % 60) * 100;
+        rpmMeter.SetText("Rpm : " + rpm);
+        }
+
+        Debug.Log("wheelsOnRaod : " + wheelsOnRoad);
+    }
+
+    bool IsOnRoad()
+    {
+        wheelsOnRoad = 0;
+        foreach (WheelCollider wheel in wheels)
+        {
+            if (wheel.isGrounded)
+            {
+                wheelsOnRoad++;
+            }
+        }
+
+        if (wheelsOnRoad > 0)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
     }
 }
